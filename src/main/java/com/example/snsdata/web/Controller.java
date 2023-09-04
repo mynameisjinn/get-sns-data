c94fca570a2a85867090082be60a9fb015f05b9a
hyejin <obndo8539@naver.com>
2023-09-04 13:07:51 +0900

    Instagram, Facebook 로그인한 유저의 게시물 받아오기 성공

---
+++ src/main/java/com/example/snsdata/web/Controller.java
@@ -0,0 +1,23 @@
+package com.example.snsdata.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping("")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/facebook")
+    public String facebook(){
+        return "facebook";
+    }
+
+    @GetMapping("/instagram")
+    public String instagram(){
+        return "instagram";
+    }
+}
